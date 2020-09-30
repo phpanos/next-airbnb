7a07635d70a63fb397ff9218b7dfe80c33dc02ff
phpanos <phpanos@gmail.com>
2020-10-01 00:20:24 +0200

    Init project

---
+++ components/images/Logo.js
@@ -0,0 +1,5 @@
+const Logo = () => <div>
+    <img src={ require('../../public/img/logo.svg') } className="w-6 h-6" />
+</div>
+
+export default Logo
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: ['./pages/*.js', './components/*.js'],
+  theme: {
+    backgroundPosition: {
+      'center-center': '50% 50%',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
